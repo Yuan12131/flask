16a956348e009ce5b794380b4d7952879423ecb2
leeyuan <dbdks32123@gmail.com>
2023-10-26 12:07:55 +0900

    flask로 서버 생성

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# GET 요청 처리: index.html을 클라이언트에 전달
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+# POST 요청 처리: 받은 데이터를 터미널에 출력
+@app.route('/submit', methods=['POST'])
+def submit():
+    input_data = request.form['inputData']  # input 요소의 name 속성을 사용
+    print('Received data:', input_data)
+    return 'Data received successfully.'
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=3000)
