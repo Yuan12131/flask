3274fafb3ac59f4f72a0a57608718b0d6583db6a
leeyuan <dbdks32123@gmail.com>
2024-02-20 19:25:02 +0900

    pyhton - flask get, post

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# POST 요청 처리: 받은 데이터를 터미널에 출력
+# POST 요청 처리: 받은 데이터를 터미널에 출력하고 클라이언트에 전달
 @app.route('/submit', methods=['POST'])
 def submit():
     input_data = request.form['inputData']  # input 요소의 name 속성을 사용
     print('Received data:', input_data)
-    return 'Data received successfully.'
+    return render_template('index.html', result=input_data)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=3000)
