e29061be722fff2e09d84ef919d5126fe49fea44
leeyuan <dbdks32123@gmail.com>
2024-02-21 14:14:07 +0900

    DB 로직

--- app.py
+++ app.py
@@ -1,18 +1,60 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/flasktest'
+app.config['SECRET_KEY'] = 'your_secret_key'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
-# GET 요청 처리: index.html을 클라이언트에 전달
-@app.route('/', methods=['GET'])
-def index():
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username, password=password).first()
+
+        if user:
+            login_user(user)
+            flash('로그인 성공', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('아이디 또는 비밀번호가 맞지 않습니다.', 'danger')
+
+    return render_template('login.html')
+
+
+@app.route('/')
+def home():
     return render_template('index.html')
 
-# POST 요청 처리: 받은 데이터를 터미널에 출력하고 클라이언트에 전달
-@app.route('/submit', methods=['POST'])
-def submit():
-    input_data = request.form['inputData']  # input 요소의 name 속성을 사용
-    print('Received data:', input_data)
-    return render_template('index.html', result=input_data)
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', username=current_user.username)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return 'You are now logged out.'
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=3000)
+    with app.app_context():
+        db.create_all()
+    app.run(host='localhost', port=8080)
